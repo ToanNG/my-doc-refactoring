908c0aef80762a7d1dd0652f015138d26e74b5d7
Toan Nguyen <toan.nguyen@2359media.com.vn>
2014-11-09 22:43:11 +0700

    add login and signup dialogs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,4 +2,37 @@ $(function(){
   $('#menu-btn').click(function(){
     $(this).toggleClass('active');
   });
+
+  $('.show-patients-login-dialog').click(function(){
+    showPatientsLoginDialog();
+  });
+  $('.show-professionals-login-dialog').click(function(){
+    showProfessionalsLoginDialog();
+  });
+  $('.show-patients-signup-dialog').click(function(){
+    showPatientsSignupDialog();
+  });
+  $('.show-professionals-signup-dialog').click(function(){
+    showProfessionalsSignupDialog();
+  });
 });
+
+function showPatientsLoginDialog(){
+  $('.modal').modal('hide');
+  $('#patients-login-dialog').modal('show');
+}
+
+function showProfessionalsLoginDialog(){
+  $('.modal').modal('hide');
+  $('#professionals-login-dialog').modal('show');
+}
+
+function showPatientsSignupDialog(){
+  $('.modal').modal('hide');
+  $('#patients-signup-dialog').modal('show');
+}
+
+function showProfessionalsSignupDialog(){
+  $('.modal').modal('hide');
+  $('#professionals-signup-dialog').modal('show');
+}
